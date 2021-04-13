3346edb1f1923d5b44ee651b165c843bd1f36e7c
Mika Schmeling <Mikaela.schmeling@gmail.com>
2021-04-13 17:44:33 +0100

    Rating Box Plot!
    Took 39 minutes

--- ratingBoxplot.js
+++ ratingBoxplot.js
@@ -1,48 +1,93 @@
+
+// modified from: https://www.d3-graph-gallery.com/graph/boxplot_several_groups.html
+// particularly: changed the nest function to the updated group function to make it work properly
+// keys no longer accessed, it is d[0] instead of d.key and d[1] instead of d.value
+
 window.renderRuntimeBoxplot = function (view) {
 
     const ratings = formatData(view)
-    console.log(ratings)
 
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
+    // append the svg object to the body of the page
+    var svg = d3.select("#ratingBoxplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    chart = {
-        // // set the height and width used to create the visualisation but not as seen on screen
-        // // get the svg and set the size of the view box
-        // // TODO: Note this currently isnt stored as a svg so you may need to make one, its just a div rn
+    const sumstat = d3.rollup(ratings, (d) => {
+        q1 = d3.quantile(d.map(function(g) { return g.imdb;}).sort(d3.ascending),.25)
+        median = d3.quantile(d.map(function(g) { return g.imdb;}).sort(d3.ascending),.5)
+        q3 = d3.quantile(d.map(function(g) { return g.imdb;}).sort(d3.ascending),.75)
+        interQuantileRange = q3 - q1
+        min = q1 - 1.5 * interQuantileRange
+        max = q3 + 1.5 * interQuantileRange
+        return({q1: q1, median: median, q3: q3, interQuantileRange: interQuantileRange, min: min, max: max})
+    }, (k) => {
+        return k.platform
+    })
 
-    }
+    // Show the X scale
+    var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(["netflix", "hulu", "disney", "prime"])
+        .paddingInner(1)
+        .paddingOuter(.5)
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
 
-    // boxes: set the min, max, q1-q3
-    boxes = {
-        let arrMap = Array.from(d3.group(ratings, d => d.platform), ([platform, imdb]) => ({
-            platform,
-            imdb
-        }));
-        arrMap.map(o => {
-            const values = o.imdb.map(d => d.imdb);
-            const min = d3.min(values);
-            const max = d3.max(values);
-            const q1 = d3.quantile(values, .25);
-            const q2 = d3.quantile(values, .5);
-            const q3 = d3.quantile(values, .75);
-            const iqr = q3 - q1;
-            const r0 = Math.max(min, q1 - iqr * 1);
-            const r1 = Math.min(max, q3 + iqr * 1.5);
-            o.quartiles = [q1, q2, q3];
-            o.range = [r0, r1];
-            o.outliers = values.filter(v => v < r0 || v > r1);
-            return o;
-        });
+    // Show the Y scale
+    var y = d3.scaleLinear()
+        .domain([0,10])
+        .range([height, 0])
+    svg.append("g").call(d3.axisLeft(y))
 
-        return arrMap.sort((a, b) => keys.indexOf(a.key) - keys.indexOf(b.key));
-    }
+    // Show the main vertical line
+    svg
+        .selectAll("vertLines")
+        .data(sumstat)
+        .enter()
+        .append("line")
+        .attr("x1", function(d){return(x(d[0]))})
+        .attr("x2", function(d){return(x(d[0]))})
+        .attr("y1", function(d){return(y(d[1].min))})
+        .attr("y2", function(d){return(y(d[1].max))})
+        .attr("stroke", "black")
+        .style("width", 40)
 
+    // rectangle for the main box
+    var boxWidth = 100
+    svg
+        .selectAll("boxes")
+        .data(sumstat)
+        .enter()
+        .append("rect")
+        .attr("x", function(d){return(x(d[0])-boxWidth/2)})
+        .attr("y", function(d){return(y(d[1].q3))})
+        .attr("height", function(d){return(y(d[1].q1)-y(d[1].q3))})
+        .attr("width", boxWidth )
+        .attr("stroke", "black")
+        .style("fill", "#69b3a2")
 
-
-
-    // keys = [...new Set(ratings.map(x => x.platform))]
-    // console.log(keys);
-
+    // Show the median
+    svg
+        .selectAll("medianLines")
+        .data(sumstat)
+        .enter()
+        .append("line")
+        .attr("x1", function(d){return(x(d[0])-boxWidth/2) })
+        .attr("x2", function(d){return(x(d[0])+boxWidth/2) })
+        .attr("y1", function(d){return(y(d[1].median))})
+        .attr("y2", function(d){return(y(d[1].median))})
+        .attr("stroke", "black")
+        .style("width", 80)
 
 
 }
