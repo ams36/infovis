169d1fda8164f847f5e3fda9c1cec4a598ea66fc
Mika Schmeling <Mikaela.schmeling@gmail.com>
2021-04-17 22:02:20 +0100

    html css
    Took 41 minutes

--- script/languages.js
+++ script/languages.js
@@ -172,8 +172,9 @@ window.renderLanguages = function (view) {
     }
 
     let nextPageButton = document.createElement("button")
+    nextPageButton.classList = 'btn-floating btn-large waves-effect waves-light red'
     nextPageButton.id = "languageNextPage"
-    nextPageButton.innerText = "Next Page"
+    nextPageButton.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M6,13V11H14L10.5,7.5L11.92,6.08L17.84,12L11.92,17.92L10.5,16.5L14,13H6M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12Z" /></svg>`
     nextPageButton.onclick = () => {
         if ((currentPage * points) + points  > getSelectedList().length) return
         currentPage ++
@@ -181,6 +182,7 @@ window.renderLanguages = function (view) {
     }
 
     let previousPageButton = document.createElement("button")
+    previousPageButton.classList = 'btn-floating btn-large waves-effect waves-light red'
     previousPageButton.id = "languagePreviousPage"
     previousPageButton.innerText = "Previous Page"
     previousPageButton.onclick = () => {
